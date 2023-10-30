126bb9e06b857157450336c4d4ad464500493225
Andrew Dixon <andrewdixon_97@hotmail.co.uk>
2023-10-30 22:38:16 +0000

    added simple HTTP GET root API route

--- api/server/api.js
+++ api/server/api.js
@@ -4,12 +4,16 @@ const schema = require('./schema/schema');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello World! The API is at /api')
+});
+
 app.use('/api', graphqlHTTP({
-  schema: schema,
-  graphiql: true,
+    schema: schema,
+    graphiql: true,
 }));
 
 const port = 4000;
 app.listen(port, () => {
     console.log(`Node.js Express server running... \nVisit http://localhost:${port}/api to query`)
-  })
+});
